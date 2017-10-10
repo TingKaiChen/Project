4083c8abd2d2971874c5d2f6f9275c3ca741a76f
TingKaiChen <r05921063@ntu.edu.tw>
2017-10-11 00:15:17 +0800

    Rotation error caused by order of multiplication

--- gridMapICP/GlobalMap_3D.m
+++ gridMapICP/GlobalMap_3D.m
@@ -14,37 +14,39 @@ axis equal
 hold on
 
 % Read in CSV file and seperate the data
-csvfilename='../read CPEV data/CPEV170523/CPEV_Record_2017_05_23_16_22_21.mat';
-mapfilename='./globalmap/wallcloud_20170522132724.mat';
-pgfilename='./pose_graph/pose_20170523162617_bugfix.mat';
+csvfilename ='../read CPEV data/CPEV170523/CPEV_Record_2017_05_23_16_22_21.mat';
+mapfilename ='./globalmap/wallcloud_20170522132724.mat';
+pgfilename  ='./pose_graph/edge_20170522133150.mat';
 
 
-load('../read CPEV data/CPEV170523/CPEV_Record_2017_05_23_16_26_17.mat')
+load('../read CPEV data/CPEV170522/CPEV_Record_2017_05_22_13_31_50.mat')
 
 step = 1;
-wr = 0.1;
-dr = 3.0;
+wr   = 0.1;
+dr   = 3.0;
 
 % Initial pose
-% rotd1 = eul2rotm([deg2rad(-3.6),0,0]);
-rotd1 = eul2rotm([0,0,0]);
-% trajd1 = [35.2;46.1;0];
-trajd1 = [0;0;0];
-wallcloud = [];
-wallcolor = 'b';
-trajcolor = 'k';
-bfd1 = [];
+% rotd1    = eul2rotm([deg2rad(-3.6),0,0]);
+rotd1      = eul2rotm([0,0,0]);
+% trajd1   = [35.2;46.1;0];
+trajd1     = [0;0;0];
+wallcloud  = [];
+wallcolor  = 'b';
+trajcolor  = 'k';
+bfd1       = [];
 trajectory = [];
-timetable = [];
-visible = false;
+timetable  = [];
+visible    = false;
 
-vertex = [0, trajd1', rotm2quat(rotd1)];    % [id,tx,ty,tz,q1,q2,q3,q4]
-edges = []; % [id1,id2,tx,ty,tz,q1,q2,q3,q4]
+quat   = rotm2quat(rotd1);
+vertex = [0, trajd1', quat(2:end), quat(1)];    % [id,tx,ty,tz,q1,q2,q3,q4]
+edges  = []; % [id1,id2,tx,ty,tz,q1,q2,q3,q4]
 
 % tic
-t_all = tic;
-it = 1;
+t_all   = tic;
+it      = 1;
 edge_it = 1;
+e_id    = 0;
 for frame=1:step:(m/16)
     t_iter = tic;
     iter = 50;
@@ -82,30 +84,61 @@ for frame=1:step:(m/16)
 
     % ICP
     P_src = [x1_a,x2_a,x3_a,x4_a;y1_a,y2_a,y3_a,y4_a;z1_a,z2_a,z3_a,z4_a];
+    T1 = [rotd1 trajd1;0 0 0 1];
+    R1 = rotd1;
 
     if frame ~= 1
-        initRot = rotd1;
-        initPos = trajd1;
+        initRot   = rotd1;
+        initPos   = trajd1;
         P_src_tmp = P_src;
         % Initial condition
 
         wcdist = sum((wallcloud-trajd1).^2,1).^0.5;
-        wctmp = wallcloud(:,wcdist<=100);
-        P_src_tmp=initRot*P_src_tmp+initPos*ones(1,size(P_src_tmp,2));
-        P_src=initRot*P_src+initPos*ones(1,size(P_src,2));
+        wctmp  = wallcloud(:,wcdist<=100);
+        P_src_tmp = initRot*P_src_tmp+initPos*ones(1,size(P_src_tmp,2));
+        P_src     = initRot*P_src+initPos*ones(1,size(P_src,2));
         [TRd1,TTd1]=icp(wctmp,P_src_tmp,iter,'Matching','kDtree','WorstRejection',wr);
         initRot = TRd1*initRot;
         initPos = TRd1*initPos+TTd1;
-        P_src_tmp=TRd1*P_src_tmp+TTd1*ones(1,size(P_src_tmp,2));
-        P_src=TRd1*P_src+TTd1*ones(1,size(P_src,2));
+        R12  = TRd1;
+        edge_t  = TTd1;
+
+        if sum(abs(rotm2eul(R1*R12/initRot))>1e-3*[1 1 1])>0
+            disp('-----------1st ICP--------------')
+            disp('Rotation error (rad): R1*R12*R2^T')
+            rotm2eul(R1*R12*initRot')
+            disp('Rotation error (rad): R2^T*R12*R1')
+            rotm2eul(initRot'*R12*R1)
+            % break
+        end
+
+        P_src_tmp =TRd1*P_src_tmp+TTd1*ones(1,size(P_src_tmp,2));
+        P_src     =TRd1*P_src+TTd1*ones(1,size(P_src,2));
         [TRd1,TTd1,p_indxd1,q_indxd1]=icpMatch(wctmp,P_src_tmp,iter,'Matching','kDtree',...
             'WorstRejection',dr,'UnmatchDistance',0.5);
         initRot = TRd1*initRot;
         initPos = TRd1*initPos+TTd1;
+        R12  = TRd1*R12;
+        % edge_t  = TRd1*edge_t+TTd1;
+        % edge_t  = edge_t+(R12-eye(3,3))*trajd1;
+        % edge_t  = rotd1'*edge_t;
+        edge_t  = rotd1'*(initPos-trajd1);
 
-        P_src=TRd1*P_src+TTd1*ones(1,size(P_src,2));
-        rotd1 = initRot;
+        if sum(abs(rotm2eul(R1*R12/initRot))>1e-3*[1 1 1])>0
+            disp('-----------2nd ICP--------------')
+            disp('Rotation error (rad): R1*R12*R2^T')
+            rotm2eul(R1*R12*initRot')
+            disp('Rotation error (rad): R2^T*R12*R1')
+            rotm2eul(initRot'*R12*R1)
+            % break
+        end
+
+        P_src  =TRd1*P_src+TTd1*ones(1,size(P_src,2));
+        rotd1  = initRot;
         trajd1 = initPos;
+
+        
+
 
         % 
         % P_src=rotd1*P_src+trajd1;
@@ -135,6 +168,8 @@ for frame=1:step:(m/16)
         wallcloud = rotd1*P_src+trajd1;
         wallcloud = round(wallcloud*10)/10;
         wc=scatter3(wallcloud(1,:),wallcloud(2,:),wallcloud(3,:),3,linspace(1,10,size(wallcloud,2)),'filled');
+        R12 = rotd1;
+        edge_t = trajd1;
 
         % wcdist = sum((wallcloud-[trajd1;0]).^2,1).^0.5;
         % wctmp = wallcloud(:,wcdist<=100);
@@ -147,11 +182,19 @@ for frame=1:step:(m/16)
     end 
 
     % Construct pose-graph
-    quat = rotm2quat(rotd1);
-    vertex(it+1,:) = [it, trajd1', quat(2:end), quat(1)];
-    % vertex(it+1,:) = [it, trajd1', quat];
-    edges(edge_it,:) = [0, it, trajd1', quat(2:end), quat(1)];
-    % edges(edge_it,:) = [0, it, trajd1', quat];
+    v_quat = rotm2quat(rotd1);
+    e_quat = rotm2quat(R12);
+    vertex(it+1,:) = [it, trajd1', v_quat(2:end), v_quat(1)];
+    edges(edge_it,:) = [e_id, it, edge_t', e_quat(2:end), e_quat(1)];
+
+    T2 = [rotd1 trajd1;0 0 0 1];
+    T12 = [R12 edge_t;0 0 0 1];
+    E = T12^-1*T1^-1*T2;
+    % if sum(E(1:3,end)>(1e-5*[1;1;1])) > 0 | sum(abs(rotm2eul(E(1:3,1:3)))>(1e-3*[1 1 1])) > 0
+    %     break
+    % end
+    % R2 = rotd1;
+    % rotm2eul(R12'*R1'*R2)
 
 
     if visible
@@ -176,7 +219,7 @@ for frame=1:step:(m/16)
     end
 
 
-    disp(frame)
+    disp(['Iteration: ', num2str(frame)])
     % if frame >100
     %     % waitforbuttonpress;
     %     break
@@ -184,6 +227,7 @@ for frame=1:step:(m/16)
 
     timetable(it)=toc(t_iter);
 
+    e_id = it;
     it = it+1;
     edge_it = edge_it+1;
     preframe = frame;
@@ -202,7 +246,7 @@ end
 % save(mapfilename,'wallcloud','trajectory')
 
 % % Save the pose-graph
-save(pgfilename,'vertex','edges')
+% save(pgfilename,'vertex','edges')
 
 
 disp('END')
